619e5e3203eb7b2181a33e77f7180060cc341fe4
Vinicius <gering@thidlevel.com.br>
2016-10-06 14:16:06 -0300

    subindo OC Vinicius

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8080/opencart_vini/admin/');
+define('HTTP_CATALOG', 'http://localhost:8080/opencart_vini/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8080/opencart_vini/admin/');
+define('HTTPS_CATALOG', 'http://localhost:8080/opencart_vini/');
+
+// DIR
+define('DIR_APPLICATION', '/Applications/mampstack-5.6.26-2/apache2/htdocs/opencart_vini/admin/');
+define('DIR_SYSTEM', '/Applications/mampstack-5.6.26-2/apache2/htdocs/opencart_vini/system/');
+define('DIR_IMAGE', '/Applications/mampstack-5.6.26-2/apache2/htdocs/opencart_vini/image/');
+define('DIR_LANGUAGE', '/Applications/mampstack-5.6.26-2/apache2/htdocs/opencart_vini/admin/language/');
+define('DIR_TEMPLATE', '/Applications/mampstack-5.6.26-2/apache2/htdocs/opencart_vini/admin/view/template/');
+define('DIR_CONFIG', '/Applications/mampstack-5.6.26-2/apache2/htdocs/opencart_vini/system/config/');
+define('DIR_CACHE', '/Applications/mampstack-5.6.26-2/apache2/htdocs/opencart_vini/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Applications/mampstack-5.6.26-2/apache2/htdocs/opencart_vini/system/storage/download/');
+define('DIR_LOGS', '/Applications/mampstack-5.6.26-2/apache2/htdocs/opencart_vini/system/storage/logs/');
+define('DIR_MODIFICATION', '/Applications/mampstack-5.6.26-2/apache2/htdocs/opencart_vini/system/storage/modification/');
+define('DIR_UPLOAD', '/Applications/mampstack-5.6.26-2/apache2/htdocs/opencart_vini/system/storage/upload/');
+define('DIR_CATALOG', '/Applications/mampstack-5.6.26-2/apache2/htdocs/opencart_vini/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'viglobal');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8080/opencart_vini/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8080/opencart_vini/');
+
+// DIR
+define('DIR_APPLICATION', '/Applications/mampstack-5.6.26-2/apache2/htdocs/opencart_vini/catalog/');
+define('DIR_SYSTEM', '/Applications/mampstack-5.6.26-2/apache2/htdocs/opencart_vini/system/');
+define('DIR_IMAGE', '/Applications/mampstack-5.6.26-2/apache2/htdocs/opencart_vini/image/');
+define('DIR_LANGUAGE', '/Applications/mampstack-5.6.26-2/apache2/htdocs/opencart_vini/catalog/language/');
+define('DIR_TEMPLATE', '/Applications/mampstack-5.6.26-2/apache2/htdocs/opencart_vini/catalog/view/theme/');
+define('DIR_CONFIG', '/Applications/mampstack-5.6.26-2/apache2/htdocs/opencart_vini/system/config/');
+define('DIR_CACHE', '/Applications/mampstack-5.6.26-2/apache2/htdocs/opencart_vini/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Applications/mampstack-5.6.26-2/apache2/htdocs/opencart_vini/system/storage/download/');
+define('DIR_LOGS', '/Applications/mampstack-5.6.26-2/apache2/htdocs/opencart_vini/system/storage/logs/');
+define('DIR_MODIFICATION', '/Applications/mampstack-5.6.26-2/apache2/htdocs/opencart_vini/system/storage/modification/');
+define('DIR_UPLOAD', '/Applications/mampstack-5.6.26-2/apache2/htdocs/opencart_vini/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'viglobal');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
